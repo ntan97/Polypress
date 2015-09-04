7b2b0a07da4239af3abe5a568576f00d1fe92c79
Nathan Gould <ntan97@gmail.com>
2015-09-03 20:00:37 -0600

    Added better item output and more consistent colors

--- inc/wp_polymer_nav.php
+++ inc/wp_polymer_nav.php
@@ -46,7 +46,14 @@ function start_lvl( &$output, $depth = 0, $args = Array() ) {
     $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
     $attributes .= ' class="menu-link ' . ( $depth > 0 ? 'sub-menu-link' : 'main-menu-link' ) . '"';
 
-    $item_output = '';
+    $item_output = sprintf( '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
+        $args->before,
+        $attributes,
+        $args->link_before,
+        apply_filters( 'the_title', $item->title, $item->ID ),
+        $args->link_after,
+        '</paper-item>'
+    );
 
     // build html
     $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
